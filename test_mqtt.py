78d706deb837fd6cda1b32981b0335ebb75f4480
IndiePotato <OliverRobertNock@Gmail.com>
2019-12-19 17:35:10 +0000

    Initial client file.

---
+++ test_mqtt.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt_client
+
+broker_url = "localhost"
+broker_port = 1883
+
+client = mqtt_client.Client()
+client.connect(broker_url)
+
+def on_message(client, userdata, message):
+    print(f"Message received: {0}", str(message.payload.decode("utf-8")))
+    
+client.on_message = on_message    
+
+print("Test Subscribe")
+client.subscribe("luxReadings", qos=0)
+
+print("Test Publish")
+client.publish("luxReadings", 450.42, qos=0, retain=False)
+
