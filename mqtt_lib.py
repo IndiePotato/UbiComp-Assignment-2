9c5dee5eb8d15880b3d064f00af6cf788b8f6116
Oliver Nock <oliverrobertnock@gmail.com>
2020-01-05 20:13:40 +0000

    Turning mqtt_test.py to mqtt_lib.py with major refactoring.

--- mqtt_lib.py
+++ mqtt_lib.py
@@ -1,21 +1,16 @@
-import paho.mqtt.client as mqtt_client
-
-broker_url = "127.0.0.1"
-broker_port = 1883
-
-client = mqtt_client.Client()
-client.connect(broker_url)
+"""
+Library file for MQTT-related functions for UbiComp Assignment.
+"""
 
 
-def on_message(client, userdata, message):
+def on_message(message):
+    """
+    MQTT Client Object on_message function value.
+
+    Arguments:
+        message The message received.
+    """
     print(f"Message received: {0}", str(message.payload.decode("utf-8")))
 
 
-client.on_message = on_message    
-
-print("Test Subscribe")
-client.subscribe("luxReadings", qos=0)
-
-print("Test Publish")
-client.publish("luxReadings", 450.42, qos=0, retain=False)
 
