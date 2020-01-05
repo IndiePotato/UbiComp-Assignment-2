939c0de4b13370b6b8b6c93b8952a77dfb4f02e4
Oliver Nock <oliverrobertnock@gmail.com>
2020-01-05 19:42:03 +0000

    Refactoring test_mqtt.py

--- test_mqtt.py
+++ test_mqtt.py
@@ -1,14 +1,16 @@
 import paho.mqtt.client as mqtt_client
 
-broker_url = "localhost"
+broker_url = "127.0.0.1"
 broker_port = 1883
 
 client = mqtt_client.Client()
 client.connect(broker_url)
 
+
 def on_message(client, userdata, message):
     print(f"Message received: {0}", str(message.payload.decode("utf-8")))
-    
+
+
 client.on_message = on_message    
 
 print("Test Subscribe")
