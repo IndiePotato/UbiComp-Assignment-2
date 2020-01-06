a79bf59946a18ada4d16f7ed5b4379180dd3e34b
Oliver Nock <oliverrobertnock@gmail.com>
2020-01-06 13:34:26 +0000

    Adding print statements for publishing.

--- publish_reading.py
+++ publish_reading.py
@@ -6,15 +6,11 @@ from Adafruit_IO import MQTTClient
 from config import *
 from mqtt_lib import *
 
-broker_url = "127.0.0.1"
-broker_port = 1883
-
 client = MQTTClient(USERNAME, IO_KEY)
 client.on_message = on_message
 client.on_subscribe = on_subscribe(FEED_ID)
 serial_connection = Serial("/dev/ttyACM0", 9600, timeout=1)
 
-print("Subscribing to luxReadings feed....")
 # ToDo: Add in Try...Catch... statement for error handling.
 client.subscribe(FEED_ID, qos=0)
 
@@ -23,6 +19,7 @@ while True:
     message = raw_message.decode().strip()
 
     if message:
+        print("Publishing value: ", message)
         # ToDo: Add in Try...Catch...statement for error handling.
-        client.publish(FEED_ID, message)
+        client.publish(FEED_ID, message)    
 
